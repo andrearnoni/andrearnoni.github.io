e5ad68f9fa6119fde01012914ca5fd14ddac2c15
andrearnoni <andre_arnoni@yahoo.com.br>
2025-02-07 00:47:39 -0300

    added some skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,31 +1,33 @@
-import React, { useEffect } from 'react';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-import bootstrap from '../img/bootstrap.png';
-import bulma from '../img/bulma.png';
-import css from '../img/css.png';
-import express from '../img/express.png';
-import git from '../img/git.png';
-import html from '../img/html.png';
-import js from '../img/javascript.png';
-import jest from '../img/jest.png';
-import linux from '../img/linux.png';
-import mongodb from '../img/mongodb.png';
-import mysql from '../img/mysql.png';
-import node from '../img/node.png';
-import react from '../img/react.png';
-import redux from '../img/redux.png';
-import rtl from '../img/rtl.png';
-import mocha from '../img/mocha.png';
-import pm2 from '../img/pm2.png';
-import heroku from '../img/heroku.png';
-import '../styles/Global.css';
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import bootstrap from "../img/bootstrap.png";
+import bulma from "../img/bulma.png";
+import css from "../img/css.png";
+import express from "../img/express.png";
+import git from "../img/git.png";
+import html from "../img/html.png";
+import js from "../img/javascript.png";
+import jest from "../img/jest.png";
+import linux from "../img/linux.png";
+import mongodb from "../img/mongodb.png";
+import mysql from "../img/mysql.png";
+import node from "../img/node.png";
+import react from "../img/react.png";
+import redux from "../img/redux.png";
+import rtl from "../img/rtl.png";
+import mocha from "../img/mocha.png";
+import pm2 from "../img/pm2.png";
+import heroku from "../img/heroku.png";
+import java from "../img/java.png";
+import spring from "../img/spring.png";
+import tailwind from "../img/tailwind.png";
+import "../styles/Global.css";
 
 function Skills() {
-
   useEffect(() => {
-    Aos.init({duration: 1700});
-  }, [])
+    Aos.init({ duration: 1700 });
+  }, []);
 
   return (
     <section className="skills section" id="skills">
@@ -33,58 +35,193 @@ function Skills() {
       <span className="section__subtitle">Some learned technologies</span>
       <div className="skills__container container grid">
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="10" src={ react } alt="react" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="10"
+            src={react}
+            alt="react"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="10" src={ js } alt="javascript" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="10"
+            src={js}
+            alt="javascript"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="10" src={ html } alt="html" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="10"
+            src={html}
+            alt="html"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="70" src={ css } alt="css" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="70"
+            src={css}
+            alt="css"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="70" src={ git } alt="git" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="70"
+            src={git}
+            alt="git"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="70"  src={ linux } alt="linux" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="70"
+            src={linux}
+            alt="linux"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="125"  src={ redux } alt="redux" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="125"
+            src={redux}
+            alt="redux"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="125"  src={ rtl } alt="rtl" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="125"
+            src={rtl}
+            alt="rtl"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="125"  src={ jest } alt="jest" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="125"
+            src={jest}
+            alt="jest"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="180"  src={ bootstrap } alt="react" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="180"
+            src={bootstrap}
+            alt="react"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="180"  src={ bulma } alt="react" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="180"
+            src={bulma}
+            alt="react"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="180"  src={ node } alt="node" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="180"
+            src={node}
+            alt="node"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="185"  src={ mysql } alt="mysql" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="185"
+            src={mysql}
+            alt="mysql"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="185"  src={ mongodb } alt="mongodb" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="185"
+            src={mongodb}
+            alt="mongodb"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="185"  src={ express } alt="express" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="185"
+            src={express}
+            alt="express"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="190"  src={ mocha } alt="mocha" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="190"
+            src={mocha}
+            alt="mocha"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="190"  src={ heroku } alt="heroku" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="190"
+            src={heroku}
+            alt="heroku"
+          />
         </div>
         <div className="skills__img">
-          <img data-aos="fade-right" data-aos-once="true" data-aos-delay="190"  src={ pm2 } alt="pm2" />
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="190"
+            src={pm2}
+            alt="pm2"
+          />
+        </div>
+        <div className="skills__img">
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="195"
+            src={java}
+            alt="java"
+          />
+        </div>
+        <div className="skills__img">
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="195"
+            src={spring}
+            alt="spring"
+          />
+        </div>
+        <div className="skills__img">
+          <img
+            data-aos="fade-right"
+            data-aos-once="true"
+            data-aos-delay="195"
+            src={tailwind}
+            alt="tailwind"
+          />
         </div>
       </div>
     </section>
